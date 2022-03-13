4f60a83dee598535d2e1ed465a2a56e25a2f0958
winter.you <kakao_ent@kakao-Entui-MacBookPro.local>
2022-03-13 15:16:24 +0900

    feat: kafka template init

---
+++ src/main/java/spring/kafka/SpringKafkaTemplateApplication.java
@@ -0,0 +1,13 @@
+package spring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaTemplateApplication.class, args);
+    }
+
+}
